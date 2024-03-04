ad9141e25e6582a3fb09d0670d3c72b9486e2011
Nghia La <leonghiacnn@gmail.com>
2024-03-05 01:18:16 +0700

    Add remember me checkbox in login form

--- onlineshop4dvds_app/src/components/login-form.tsx
+++ onlineshop4dvds_app/src/components/login-form.tsx
@@ -8,19 +8,22 @@ import { Button } from "@/components/ui/button";
 import {
     Form,
     FormControl,
+    FormDescription,
     FormField,
     FormItem,
     FormLabel,
     FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Checkbox } from "./ui/checkbox";
 
 const passwordErrorMessage = "Mật khẩu phải chứa từ 6 - 64 kí tự";
 const emailErrorMessage = "Vui lòng nhập đúng định dạng email";
 
 const formSchema = z.object({
-    email: z.string().email({message: emailErrorMessage}),
-    password: z.string().min(6, { message: passwordErrorMessage }).max(64, { message: passwordErrorMessage })
+    email: z.string().email({ message: emailErrorMessage }),
+    password: z.string().min(6, { message: passwordErrorMessage }).max(64, { message: passwordErrorMessage }),
+    remember: z.boolean().default(false).optional(),
 });
 
 export default function LoginForm() {
@@ -30,6 +33,7 @@ export default function LoginForm() {
         defaultValues: {
             email: "",
             password: "",
+            remember: false,
         },
     });
 
@@ -40,7 +44,7 @@ export default function LoginForm() {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">               
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
                 <FormField
                     control={form.control}
                     name="email"
@@ -69,6 +73,27 @@ export default function LoginForm() {
                         </FormItem>
                     )}
                 />
+
+                <FormField
+                    control={form.control}
+                    name="remember"
+                    render={({ field }) => (
+                        <FormItem className="flex flex-row items-start space-x-3 space-y-0 pl-1">
+                            <FormControl>
+                                <Checkbox
+                                    checked={field.value}
+                                    onCheckedChange={field.onChange}
+                                />
+                            </FormControl>
+                            <div className="space-y-1 leading-none">
+                                <FormLabel>
+                                    Lưu thông tin
+                                </FormLabel>                            
+                            </div>
+                        </FormItem>
+                    )}
+                />
+
                 <Button type="submit" className="w-full">Đăng nhập</Button>
             </form>
         </Form>
