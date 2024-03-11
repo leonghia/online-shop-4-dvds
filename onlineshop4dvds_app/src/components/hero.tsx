cb6d02f439194e6b095b41fba588b1c827f0ff49
Nghia La <leonghiacnn@gmail.com>
2024-03-12 02:40:48 +0700

    Set logo font

--- onlineshop4dvds_app/src/components/hero.tsx
+++ onlineshop4dvds_app/src/components/hero.tsx
@@ -1,10 +1,13 @@
 import { Button, Spacer } from "@nextui-org/react";
+import { Bungee } from "next/font/google";
+
+const bungee = Bungee({subsets: ["latin"], weight: "400"});
 
 export default function Hero() {
     return (
         <section className="flex max-w-4xl flex-col items-center py-48">
             <div className="flex flex-col text-center">
-                <h1 className="text-4xl lg:text-6xl font-extrabold tracking-tight">Online Shop 4 <span className="tracking-tight inline from-[#FF72E1] to-[#F54C7A] bg-clip-text text-transparent bg-gradient-to-b">DVDS</span></h1>
+                <h1 className={`text-4xl lg:text-6xl font-extrabold tracking-tight ${bungee.className}`}>Online Shop 4 <span className="tracking-tight inline from-[#FF72E1] to-[#F54C7A] bg-clip-text text-transparent bg-gradient-to-b">DVDS</span></h1>
                 <Spacer y={4} />
                 <h2 className="text-large text-default-500">
                     Our mission is to provide a sanctuary for CD lovers, a place where they can discover new albums, revisit old movies, and preserve the rich heritage of physical media in the digital age.
