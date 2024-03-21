5e3736580d7e3a2e68653359cb14aaad88faf389
Nghia La <leonghiacnn@gmail.com>
2024-03-21 13:12:58 +0700

    Modify Cart entity

--- onlineshop4dvds_api/DTOs/CartDto.cs
+++ onlineshop4dvds_api/DTOs/CartDto.cs
@@ -5,7 +5,4 @@ public class CartDto
     public required int Id {get;set;}
     public required ICollection<CartItemDto> Items {get;set;}
     public required decimal Subtotal {get;set;}
-    public required decimal Discount {get;set;} = 0;
-    public required decimal ShippingFee {get;set;} = 0;
-    public required decimal Total {get;set;}
 }
