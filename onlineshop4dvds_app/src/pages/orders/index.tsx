b843dbe6be2afca74d3076fcec0194adb2ac5f70
Nghia La <leonghiacnn@gmail.com>
2024-03-23 07:48:07 +0700

    Crafting order cards

--- onlineshop4dvds_app/src/pages/orders/index.tsx
+++ onlineshop4dvds_app/src/pages/orders/index.tsx
@@ -1,5 +1,5 @@
 import PageLayout from "@/components/layouts/page-layout";
-import { Tabs, Tab, Card, CardBody, CardHeader, CardFooter, Divider, Link, Image } from "@nextui-org/react";
+import { Tabs, Tab, Card, CardBody, CardHeader, CardFooter, Divider, Link, Image, Chip } from "@nextui-org/react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { useEffect, useState } from "react";
 import { API_URL } from "@/config";
@@ -14,8 +14,8 @@ export default function Page() {
         fetch(`${API_URL}/order?sub=${user.sub}`)
             .then(res => res.json())
             .then((orders: Order[]) => setOrders(orders))
-            .catch(err => console.error(err));  
-    }, []);
+            .catch(err => console.error(err));
+    }, [user]);
 
     return (
         <PageLayout>
@@ -23,13 +23,37 @@ export default function Page() {
                 <div className="max-w-3xl h-full w-full px-2 space-y-6">
                     <h1 className="text-2xl font-extrabold">Orders History</h1>
                     <div className="flex w-full flex-col">
-                        <Tabs aria-label="Options">
+                        <Tabs aria-label="Options" classNames={{ tabList: "w-full", panel: "px-0" }}>
                             <Tab key="all" title="All Orders">
-                                <Card>
-                                    <CardBody>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                    </CardBody>
-                                </Card>
+                                <div className="space-y-4">
+                                    {orders?.map(o => (
+                                        <Card>
+                                            <CardHeader className="flex justify-between">
+                                                <div className="flex gap-x-2 items-center">
+                                                    <p className="text-default-500 font-medium text-small">Order ID</p>
+                                                    <Chip color="primary" variant="flat">{o.orderId}</Chip>
+                                                </div>
+                                                <p className="text-small text-default-500 font-medium">{o.status}</p>
+                                            </CardHeader>
+                                            <Divider />
+                                            <CardBody className="space-y-4">
+                                                {o.items.map(i => (
+                                                    <div className="flex justify-between">
+                                                        <div className="flex gap-x-2">
+                                                            <Image src={i.thumbnailUrl} removeWrapper classNames={{ img: "w-20 h-20 object-contain" }} alt={i.title} />
+                                                            <div>
+                                                                <h4 className="text-medium font-semibold">
+                                                                    {i.title}
+                                                                </h4>
+                                                                <p className="text-small text-default-500">x {i.quantity}</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                ))}
+                                            </CardBody>
+                                        </Card>
+                                    ))}
+                                </div>
                             </Tab>
                             <Tab key="awaiting" title="Awaiting Payment">
                                 <Card>
