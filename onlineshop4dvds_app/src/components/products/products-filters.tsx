ea7f834b102b2dace6a914d5c6d8d3d0768de5b8
Nghia La <leonghiacnn@gmail.com>
2024-03-26 11:54:34 +0700

    Create product type filter

--- onlineshop4dvds_app/src/components/products/products-filters.tsx
+++ onlineshop4dvds_app/src/components/products/products-filters.tsx
@@ -1,11 +1,22 @@
-import { Button, Popover, PopoverTrigger, PopoverContent, Slider, Input, Divider } from "@nextui-org/react";
-import { useState } from "react";
-import { HiChevronDown } from "react-icons/hi2";
+import { ProductType } from "@/utils/product";
+import { Button, Popover, PopoverTrigger, PopoverContent, Slider, Input, Divider, RadioGroup, Radio } from "@nextui-org/react";
+import { ReactElement, useState } from "react";
+import { HiChevronDown, HiMiniFunnel } from "react-icons/hi2";
 
 const defaultPriceRange: number[] = [0, 1000];
 
+const EmptyGenres = (): ReactElement => {
+    return (
+        <div className="p-4 flex flex-col justify-center items-center gap-2">
+            <HiMiniFunnel className="w-10 h-10 text-default-300" />
+            <p className="text-small font-medium text-default-500">Please select a specific product type</p>
+        </div>
+    );
+}
+
 export default function ProductsFilters() {
     const [priceRange, setPriceRange] = useState<number[] | number>([0, 500]);
+    const [productType, setProductType] = useState<ProductType>(ProductType.All);
 
     return (
         <header className="relative z-20 flex flex-col gap-2 rounded-medium bg-default-50 px-4 pb-3 pt-2 md:pt-3">
@@ -21,6 +32,47 @@ export default function ProductsFilters() {
                     </div>
                 </div>
                 <div className="-ml-2 flex w-full flex-wrap items-center justify-start gap-2 md:ml-0 md:justify-end">
+                    <Popover placement="bottom" classNames={{ content: "flex max-w-xs min-w-56 flex-col items-start gap-2 px-4 pt-4" }}>
+                        <PopoverTrigger>
+                            <Button variant="bordered" className="text-default-500" endContent={<HiChevronDown />}>Product Type</Button>
+                        </PopoverTrigger>
+                        <PopoverContent>
+                            <span className="mb-2 text-medium font-medium text-default-600">
+                                Product Type
+                            </span>
+                            <div className="w-full px-2">
+                                <RadioGroup value={productType?.toString()} onValueChange={(value: string) => setProductType(parseInt(value))} >
+                                    <Radio value={ProductType.Album.toString()}>{ProductType[ProductType.Album]}</Radio>
+                                    <Radio value={ProductType.Movie.toString()}>{ProductType[ProductType.Movie]}</Radio>
+                                    <Radio value={ProductType.Game.toString()}>{ProductType[ProductType.Game]}</Radio>
+                                    <Radio value={ProductType.All.toString()}>{ProductType[ProductType.All]}</Radio>
+                                </RadioGroup>
+                            </div>
+                            <Divider className="mt-3 bg-default-100" />
+                            <div className="flex w-full justify-end gap-2 py-2">
+                                <Button size="sm" variant="flat" className="font-medium" onPress={() => setProductType(ProductType.All)}>Reset</Button>
+                                <Button size="sm" variant="flat" color="primary" className="font-medium">Apply</Button>
+                            </div>
+                        </PopoverContent>
+                    </Popover>
+                    <Popover placement="bottom" classNames={{ content: "flex max-w-xs min-w-56 flex-col items-start gap-2 px-4 pt-4" }}>
+                        <PopoverTrigger>
+                            <Button variant="bordered" className="text-default-500" endContent={<HiChevronDown />}>Genres</Button>
+                        </PopoverTrigger>
+                        <PopoverContent>
+                            <span className="mb-2 text-medium font-medium text-default-600">
+                                Genres
+                            </span>
+                            <div className="w-full px-2">
+                                {productType === ProductType.All && <EmptyGenres />}
+                            </div>
+                            {productType !== ProductType.All && <><Divider className="mt-3 bg-default-100" />
+                                <div className="flex w-full justify-end gap-2 py-2">
+                                    <Button size="sm" variant="flat" className="font-medium" onPress={() => setProductType(ProductType.All)}>Reset</Button>
+                                    <Button size="sm" variant="flat" color="primary" className="font-medium">Apply</Button>
+                                </div></>}
+                        </PopoverContent>
+                    </Popover>
                     <Popover placement="bottom" classNames={{ content: "flex max-w-xs flex-col items-start gap-2 px-4 pt-4" }}>
                         <PopoverTrigger>
                             <Button variant="bordered" className="text-default-500" endContent={<HiChevronDown />}>Pricing Range</Button>
@@ -39,7 +91,7 @@ export default function ProductsFilters() {
                                         value={priceRange}
                                         onChange={setPriceRange}
                                         formatOptions={{ style: "currency", currency: "USD" }}
-                                        
+
                                     />
                                     <div className="flex items-center">
                                         <Input type="number" min={0} max={1000} step={50} value={(priceRange as number[])[0].toString()} onValueChange={(value: string) => setPriceRange([parseInt(value), (priceRange as number[])[1]])} labelPlacement="outside" startContent={
---
+++ onlineshop4dvds_app/src/utils/product.ts
@@ -0,0 +1,6 @@
+export enum ProductType {
+    Album,
+    Movie,
+    Game,
+    All
+}
