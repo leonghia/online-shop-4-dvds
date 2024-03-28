b05c8991a8a6123aea30aafc56d7c107a981778c
Nghia La <leonghiacnn@gmail.com>
2024-03-28 21:53:59 +0700

    Single selection in tracks table

--- onlineshop4dvds_app/src/components/tracks.tsx
+++ onlineshop4dvds_app/src/components/tracks.tsx
@@ -1,16 +1,19 @@
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
-import { HiOutlineClock } from "react-icons/hi2";
+import { Key, useState } from "react";
+import { HiOutlineClock, HiMiniPlay } from "react-icons/hi2";
 
 export default function Tracks({ albumId }: { albumId?: string }) {
+    const [rowHovered, setRowHovered] = useState<number | null>(null);
+
     return (
-        <Table removeWrapper aria-label="Example static collection table" selectionMode="single" color="primary">
+        <Table removeWrapper aria-label="Example static collection table" color="primary" selectionMode="single">
             <TableHeader>
-                <TableColumn width={1}>#</TableColumn>
+                <TableColumn width="6%">#</TableColumn>
                 <TableColumn>TITLE</TableColumn>
                 <TableColumn width={1}><HiOutlineClock className="w-5 h-5" /></TableColumn>
             </TableHeader>
             <TableBody>
-                <TableRow key="1">
+                <TableRow key="1" onMouseEnter={(e) => setRowHovered(1)} onMouseLeave={(e) => setRowHovered(null)}>
                     <TableCell className="text-default-500">1</TableCell>
                     <TableCell>
                         <p>Welcome To New York (Taylor's Version)</p>
@@ -18,26 +21,10 @@ export default function Tracks({ albumId }: { albumId?: string }) {
                     </TableCell>
                     <TableCell className="text-default-500">3:32</TableCell>
                 </TableRow>
-                <TableRow key="2">
+                <TableRow key="2" onMouseEnter={(e) => setRowHovered(2)} onMouseLeave={(e) => setRowHovered(null)}>
                     <TableCell className="text-default-500">2</TableCell>
                     <TableCell>
                         <p>Blank Space (Taylor's Version)</p>
-                        <p className="text-default-500">Taylor Swift</p>
-                    </TableCell>
-                    <TableCell className="text-default-500">3:32</TableCell>
-                </TableRow>
-                <TableRow key="3">
-                    <TableCell className="text-default-500">3</TableCell>
-                    <TableCell>
-                        <p>Style (Taylor's Version)</p>
-                        <p className="text-default-500">Taylor Swift</p>
-                    </TableCell>
-                    <TableCell className="text-default-500">3:32</TableCell>
-                </TableRow>
-                <TableRow key="4">
-                    <TableCell className="text-default-500">4</TableCell>
-                    <TableCell>
-                        <p>Out Of The Woods (Taylor's Version)</p>
                         <p className="text-default-500">Taylor Swift</p>
                     </TableCell>
                     <TableCell className="text-default-500">3:32</TableCell>
